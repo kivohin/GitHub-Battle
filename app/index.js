9a650ec880a9fec5996d6f72cd45c182b48a36cb
Anthony Nabuurs <kivohin@protonmail.com>
2016-11-05 20:21:31 -0400

    Add missing React components to previous section 'Pure Functions. f(d)=v. Props and Nesting React Components' And add Link component from next video.

--- app/index.js
+++ app/index.js
@@ -1,15 +1,63 @@
+var USER_DATA = {
+  name: 'Goku Queen',
+  username: 'kivohin',
+  image: 'https://avatars2.githubusercontent.com/u/17774060?v=3&s=400'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
   render: function() {
-    return (
-      <div>Hello {this.props.name}!</div>
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}}/>
+  }
+});
+
+var Link = React.createClass({
+  changeUrl: function() {
+    window.location.replace(this.props.href)
+  },
+
+  render: function() {
+    return(
+      <span
+        style={{color: 'blue', cursor: 'pointer'}}
+        onClick={this.changeUrl}>
+        {this.props.children}
+      </span>
+    )
+  }
+});
+
+var ProfileLink = React.createClass({
+  render: function() {
+    return(
+      <div>
+        <Link href={'https://www.github.com/' + this.props.username}>{this.props.username}</Link>
+      </div>
+    )
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function() {
+    return <div>{this.props.name}</div>
+  }
+})
+
+var Avatar = React.createClass({
+  render: function() {
+    return(
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
     )
   }
 });
 
 ReactDOM.render(
-  <HelloWorld name="Boomy" />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
 );
