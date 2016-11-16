ab121baa38226eb91fee9a57e54d3ff2277204e1
Anthony Nabuurs <kivohin@protonmail.com>
2016-11-15 22:40:56 -0500

    Finish section 'More Container vs Presentational Components'.

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -9,6 +9,34 @@ function getUserInfo(username) {
   return axios.get('https://api.github.com/users/' + username + param)
 }
 
+function getRepos(username) {
+  return axios.get('https://api.github.com/users/' + username + '/repos' + param + '&per_page-100')
+}
+
+function getTotalStars(repos) {
+  return repos.data.reduce(function(prev, current) {
+    return prev + current.stargazers_count
+  }, 0)
+}
+
+function getPlayersData(player) {
+  return getRepos(player.login)
+    .then(getTotalStars)
+    .then(function(totalStars) {
+      return{
+        followers: player.followers,
+        totalStars: totalStars
+      }
+    })
+}
+
+function calculateScores(players) {
+  return[
+    players[0].followers * 3 + players[0].totalStars,
+    players[1].followers * 3 + players[1].totalStars
+  ]
+}
+
 var helpers = {
   getPlayersInfo: function(players) {
     // Fetch some data from GitHub
@@ -19,8 +47,17 @@ var helpers = {
         return user.data
       })
     }).catch(function(err) {
-      console.warn('Error in getPlayersInfo', err)
+      console.warn('Error in getPlayersInfo: ', err)
     })
+  },
+
+  battle: function(players) {
+    var PlayerOneData = getPlayersData(players[0]);
+    var PlayerTwoData = getPlayersData(players[1]);
+
+    return axios.all([PlayerOneData, PlayerTwoData])
+      .then(calculateScores)
+      .catch(function(err) {console.warn('Error in getPlayersData: ', err)})
   }
 };
 
