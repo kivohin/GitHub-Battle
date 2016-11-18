10c9133b7a5c4b540532417b9bd16d9cb4a4bdbc
Anthony Nabuurs <kivohin@protonmail.com>
2016-11-17 23:18:26 -0500

    Create 'Results' view structure and design, and calculate winner and loser.

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,13 +1,24 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
 var styles = require('../styles');
+var UserDetails = require('./UserDetails');
+var UserDetailsWrapper = require('./UserDetailsWrapper');
 
 function Results(props) {
+  var winningIndex = props.scores[0] > props.scores[1] ? 0 : 1;
+  var losingIndex = winningIndex === 0 ? 1 : 0;
+
   return(
     <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
       <h1>Results</h1>
       <div className="col-sm-8 col-sm-offset-2">
-        
+        <UserDetailsWrapper header="Winner">
+          <UserDetails score={props.scores[winningIndex]} info={props.playersInfo[winningIndex]} />
+        </UserDetailsWrapper>
+
+        <UserDetailsWrapper header="Loser">
+          <UserDetails score={props.scores[losingIndex]} info={props.playersInfo[losingIndex]} />
+        </UserDetailsWrapper>
       </div>
     </div>
   )
