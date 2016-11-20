fca467da20121290e5dc132a9764c76ae16cba3d
Anthony Nabuurs <kivohin@protonmail.com>
2016-11-19 22:21:40 -0500

    Finish section 'Building a Highly Reusable React Component'.

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -5,6 +5,7 @@ var styles = require('../styles');
 var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
 var MainContainer = require('./MainContainer');
+var Loading = require('./Loading');
 
 // var puke = function(object) {
 //   return <pre>{JSON.stringify(object, null, ' ')}</pre>
@@ -13,7 +14,7 @@ var MainContainer = require('./MainContainer');
 // Functional stateless component
 function ConfirmBattle(props) {
   return props.isLoading === true
-    ? <p>Loading...</p>
+    ? <Loading />
     : <MainContainer>
         <h1>Confirm Players</h1>
         <div className="col-sm-8 col-sm-offset-2">
--- app/components/Results.js
+++ app/components/Results.js
@@ -5,6 +5,7 @@ var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
 var Link = require('react-router').Link;
 var MainContainer = require('./MainContainer');
+var Loading = require('./Loading');
 
 function StartOver() {
   return(
@@ -19,7 +20,7 @@ function StartOver() {
 function Results(props) {
   if(props.isLoading === true) {
     return(
-      <p>Loading...</p>
+      <Loading />
     )
   }
 
