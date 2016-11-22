70ec0874914f7a31e833a8872a979b92552a7e22
Anthony Nabuurs <kivohin@protonmail.com>
2016-11-21 23:59:27 -0500

    Finish section 'React Router Transition Animation and Webpack's CSS Loader'. GitHub Battle app is now complete.

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,10 +1,18 @@
 var React = require('react');
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
+// We can do the following 'require' because of the power of Webpack
+require('../main.css');
 
 var Main = React.createClass({
   render: function() {
     return(
       <div className='main-container'>
-        {this.props.children}
+        <ReactCSSTransitionGroup
+          transitionName="appear"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}>
+            {React.cloneElement(this.props.children, {key: this.props.location.pathname})}
+        </ReactCSSTransitionGroup>
       </div>
     )
   }
